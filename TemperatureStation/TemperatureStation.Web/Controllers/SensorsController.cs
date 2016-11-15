334f9f0cdee2c601fd910e3e78d67d0c3464d4ab
Gunnar <gpeipman@hotmail.com>
2016-11-15 21:03:38 +0200

    Added controller and views for sensors

---
+++ TemperatureStation/TemperatureStation.Web/Controllers/SensorsController.cs
@@ -0,0 +1,136 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TemperatureStation.Web.Data;
+
+namespace TemperatureStation.Web.Controllers
+{
+    public class SensorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SensorsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sensors.ToListAsync());
+        }
+
+        // GET: Sensors/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sensor = await _context.Sensors.SingleOrDefaultAsync(m => m.Id == id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sensor);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Sensor sensor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sensor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(sensor);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sensor = await _context.Sensors.SingleOrDefaultAsync(m => m.Id == id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+            return View(sensor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name")] Sensor sensor)
+        {
+            if (id != sensor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sensor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SensorExists(sensor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(sensor);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sensor = await _context.Sensors.SingleOrDefaultAsync(m => m.Id == id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sensor);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var sensor = await _context.Sensors.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Sensors.Remove(sensor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool SensorExists(string id)
+        {
+            return _context.Sensors.Any(e => e.Id == id);
+        }
+    }
+}
