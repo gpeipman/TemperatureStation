d696cfbb2df5c9b4e81c16b6a78a494afe66dedb
Gunnar <gpeipman@hotmail.com>
2017-03-07 18:24:17 +0200

    Update to VS2017, added average tempetature calculator, minor bug fixes for front page

---
+++ TemperatureStation/TemperatureStation.Web/Calculators/AverageTemperatureCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using TemperatureStation.Shared.Models;
+using TemperatureStation.Web.Data;
+
+namespace TemperatureStation.Web.Calculators
+{
+    [Calculator(Name = "Average temperature", Order = 1000)]
+    public class AverageTemperatureCalculator : ICalculator
+    {
+        public bool ReturnsReading
+        {
+            get { return true; }
+        }
+
+        public double Calculate(SensorReadings readings, Measurement measurement)
+        {
+            return readings.Readings.Average(r => r.Reading);
+        }
+
+        public string DisplayValue(double value)
+        {
+            return value.ToString();
+        }
+
+        public void SetParameters(string parameters)
+        {
+        }
+    }
+}
